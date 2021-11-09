b6bd04904724f21af7585d58122ee582242aeac0
chicagoist <le.legioner@gmail.com>
2021-11-09 13:26:14 +0200

    First fix

--- pi.pl
+++ pi.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-use strict;
+
 use 5.28.0; #для того чтобы можно было использовать полезные функции из новых версий. Например, say
 use utf8;
 use open qw( :std :encoding(UTF-8) );
--- prime.pl
+++ prime.pl
@@ -2,7 +2,6 @@
 
 #https://ru.wikipedia.org/wiki/%D0%A8%D0%B5%D0%B1%D0%B0%D0%BD%D0%B3_(Unix)
 
-use strict;
 use 5.28.0; #для того чтобы можно было использовать полезные функции из новых версий. Например, say
 use utf8;
 use open qw( :std :encoding(UTF-8) );
