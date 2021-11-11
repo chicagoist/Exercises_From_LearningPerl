dbb839493d9920bb3c342ab5daaa4389cff7662a
chicagoist <le.legioner@gmail.com>
2021-11-11 20:42:59 +0200

    Сменил версию Perl с 5.28.0 на 5.10.0

---
+++ 4.1_total.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -w
+
+# Напишите пользовательскую функцию с именем total, которая
+# возвращает сумму чисел в списке. (Подсказка: пользовательская
+# функция не должна выполнять ввод/вывод; она только обрабатыQ
+# вает свои параметры и возвращает значение вызывающей стороне.)
+# Проверьте ее в приведенной ниже тестовой программе. Сумма первой группы чисел { 1 3 5 7 9 }
+# должна быть равна 25.
+
+use 5.10.0;
+use utf8;
+use open qw( :std :encoding(UTF-8) );
+use warnings;
+
+my @fred       = qw{ 1 3 5 7 9 };
+#my @fred       = (1..1000);
+my $fred_total = total(@fred);
+print "The total of \@fred is $fred_total.\n";
+print "Enter some numbers on separate lines: ";
+my $user_total = total(<STDIN>);
+print "The total of those numbers is $user_total.\n";
+
+sub total {
+
+    my $sum = 0;
+
+    foreach (@_) {
+        $sum += $_;
+    }
+
+    return $sum;
+
+}
+
+# Верный отыет из книги:
+
+# Here’s one way to do it:
+# sub total {
+# my $sum;  # private variable
+# foreach (@_) {
+# $sum += $_;
+# }
+# $sum;
+# }
---
+++ 4.2_total.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -w
+
+# Используя функцию из упражнения 4.1_total.pl, напишите
+# программу для вычисления суммы чисел от 1 до 1000.
+
+use 5.10.0;
+use utf8;
+use open qw( :std :encoding(UTF-8) );
+use warnings;
+
+print "Введите два числа. Начало и конец диапазона : \n";
+my $user_total = total(<STDIN>);
+print "Общая сумма всех чисел дипазона равна $user_total.\n";
+
+sub total {
+
+    my ( $start_number, $finish_number ) = @_;
+    my $sum = 0;
+
+    for ( my $i = $start_number ; $i <= $finish_number ; $i++ ) {
+        $sum += $i;
+
+    }
+
+    return $sum;
+
+}
+
+# Верный отыет из книги:
+
+# 2. Here's one way to do it:
+# # Remember to include &total from previous exercise!
+#
+#      print "The numbers from 1 to 1000 add up to ", total(1..1000), ".\n";
+#
+# Note that we can't call the subroutine from inside the double-quoted string, so
+# the subroutine call is another separate item being passed to print. The total
+# should be 500500, a nice round number. And it shouldn't take any noticeable
+# time at all to run this program; passing a parameter list of 1,000 values is an
+# everyday task for Perl.
