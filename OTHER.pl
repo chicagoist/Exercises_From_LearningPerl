f8851c371bb2aad1776f43a863af4c8b65dc58c3
chicagoist <le.legioner@gmail.com>
2021-11-16 22:56:48 +0200

    Изучаю 5 главу.

--- OTHER.pl
+++ OTHER.pl
@@ -5,7 +5,7 @@ use 5.10.0; #для того чтобы можно было использова
 use strict;
 
 use utf8;
-use open qw( :std :encoding(UTF-8) );
+use open qw(:std :encoding(UTF-8));
 use warnings;
 
 
@@ -63,32 +63,32 @@ use warnings;
 
 #print reverse <STDIN>; Или так
 
-sub name_of_numbers {
-
-    print "Enter some numbers from 1 to 7, one per line, then press Ctrl-D:\n";
-
-    chomp( my @enterNumbers = <> );
-
-    my @listOfNumbers;
-    my $key;
-
-    (
-        $listOfNumbers[1], $listOfNumbers[2], $listOfNumbers[3],
-        $listOfNumbers[4], $listOfNumbers[5], $listOfNumbers[6],
-        $listOfNumbers[7]
-    ) = qw( fred betty barney dino wilma pebbles bamm-bamm );
-
-    foreach $key (@enterNumbers) {
-
-        print $listOfNumbers[$key] . " ";
-
-    }
-
-    # foreach (@listOfNumbers) {
-    #     print $_ . "\n";
-    # }
-
-}
+# sub name_of_numbers {
+#
+#     print "Enter some numbers from 1 to 7, one per line, then press Ctrl-D:\n";
+#
+#     chomp( my @enterNumbers = <> );
+#
+#     my @listOfNumbers;
+#     my $key;
+#
+#     (
+#         $listOfNumbers[1], $listOfNumbers[2], $listOfNumbers[3],
+#         $listOfNumbers[4], $listOfNumbers[5], $listOfNumbers[6],
+#         $listOfNumbers[7]
+#     ) = qw( fred betty barney dino wilma pebbles bamm-bamm );
+#
+#     foreach $key (@enterNumbers) {
+#
+#         print $listOfNumbers[$key] . " ";
+#
+#     }
+#
+#     # foreach (@listOfNumbers) {
+#     #     print $_ . "\n";
+#     # }
+#
+# }
 
 #name_of_numbers();
 
@@ -178,12 +178,9 @@ sub name_of_numbers {
 # use lib '/home/legioner/perl5/lib/perl5';
 
 
-
 # my $maximum = &max(3, 5, 10, 4, 6);
 
 # sub max {
-
-    
 
 
 #     #(@_) = <>;
@@ -206,22 +203,255 @@ sub name_of_numbers {
 # }
 
 # print "\nМаксимальное значение из чисел = " . max;
-use 5.10.0; 
 
 
-running_sum( 5, 6 ); 
-running_sum( 1..3 ); 
-running_sum( 4 ); 
-sub running_sum {
-state  $sum = 0;
-state @numbers;
-foreach my $number ( @_ ) {
-push @numbers, $number;
-$sum += $number;
-}
-say "The sum of (@numbers) is $sum";
-}
+# running_sum( 5, 6 );
+# running_sum( 1..3 );
+# running_sum( 4 );
+# sub running_sum {
+# state  $sum = 0;
+# state @numbers;
+# foreach my $number ( @_ ) {
+# push @numbers, $number;
+# $sum += $number;
+# }
+# say "The sum of (@numbers) is $sum";
+# }
+#
+# while (<>) {
+#
+# }
+
+# my $fred = "ggg";
+# print $fred . "\n";
+#
+# sub larger_of_fred_or_barney {
+#     my ($fred, $barney) = @_;
+#     if ($fred > $barney) {
+#         #print "Это большее из пары чисел : ";
+#          $fred;
+#     } else {
+#         #print "Это большее из пары чисел : ";
+#          $barney;
+#     }
+#
+#     print "\n"; # возвращаемое значение этой функции 1. Из-за этого вызова.
+# }
+#print larger_of_fred_or_barney(4,44) . "\n";
+
+
+# sub max {
+#     # Сравните с &larger_of_fred_or_barney
+#     if ($_[0] > $_[1]) {
+#         $_[0];
+#     } else {
+#         $_[1];
+#     }
+# }
+
+#print max(4);
+
+#my $maximum = &max_better(3, 5, 10, 4, 6);
+#
+
+# sub quadratic {
+#
+#     my ($aa, $bb, $cc) = @_;
+#
+#     if ($aa == 0) {
+#         print "No solution!";
+#         return;
+#     }
+#
+#     my $dd = $bb * $bb - 4 * $aa * $cc;
+#     #print "\$dd - $dd\n";
+#     my $sqrt_val = sqrt abs($dd);
+#     #print "\$sqrt_val - $sqrt_val\n";
+#
+#     if ($dd > 0) {
+#
+#         #print "\$dd > 0\n";
+#         my $rootMinus1 = -$bb + $sqrt_val;
+#         my $rootPlus1 = 2 * $aa;
+#         my $rootSum1 = $rootMinus1 / $rootPlus1;
+#
+#         my $rootMinus2 = -$bb - $sqrt_val;
+#         my $rootSum2 = $rootMinus2 / $rootPlus1;
+#
+#         #print "\$rootSum1  = $rootSum1\n";
+#         #print "\$rootSum2  = $rootSum2\n";
+#
+#         if ( $rootSum1 <= $rootSum2 ) {
+#
+#             print "$rootSum1, $rootSum2\n";
+#
+#         }else {
+#
+#             print "$rootSum2, $rootSum2\n";
+#             #print "No solution!";
+#         }
+#
+#         #print $rootSum1 . ", " . $rootSum2 . "\n";
+#     }
+#     elsif ($dd == 0) {
+#
+#         my $sumNull =  (-$bb / (2 * $aa));
+#
+#         print  "$sumNull\n";
+#     }
+#     else {
+#
+#         print -$bb / (2 * $aa) . ", " . $sqrt_val . ", " . -$bb / (2 * $aa) . ", " . $sqrt_val . "\n";
+#     }
+# }
+#
+#
+#
+#
+# quadratic(-2, 0, 128);
+
+
+# my @names = qw/ fred barney betty dino wilma pebbles bam-bamm dino/;
+# my @result;
+# which_element_is("dinoa", @names);
+# sub which_element_is {
+#     my($what, @array) = @_;
+#     foreach (0..$#array) {  # Индексы элементов @array
+#         if ($what eq $array[$_]) {
+#             push @result, $_;         # Ранний возврат при успешном поиске
+#         }
+#     }
+#      -1;                    # Элемент не найден (включение return не обязательно)
+# }
+#
+# print "@result\n";
+
+
+# sub list_from_fred_to_barney {
+#
+#     my($fred, $barney) = @_;
+#
+#     if ($fred < $barney) {
+#         # Отсчет по возрастанию от $fred к $barney
+#         return $fred..$barney;
+#     } else {
+#         # Отсчет по убыванию от $fred к $barney
+#        return  reverse $barney..$fred;
+#     }
+# }
+#
+# my @list_of_numbers = list_from_fred_to_barney(21,9);
+#
+# foreach (@list_of_numbers) {
+#
+#     print wantarray ? "$_" : $_." ";
+# }
+# print "\n";
+
+
+# sub let_routine {
+#
+#     my $let = 10;
+# }
+#
+# print let_routine();
+
+
+# while (defined(my $line = <STDIN>)) {
+#     print "I saw $line";
+# }
+
+# while (defined(my $line = <>)) {
+#     chomp($line);
+#     print "It was $line that I saw!\n";
+#     print "\$ARGV one = $ARGV\n";
+# }
+
+#@ARGV   = undef;
 
 while (<>) {
+    #работает
+    chomp;
+    open BEDROCK, '>>', 'logfile' or die;
+
+    print(BEDROCK "$_\n");
+    #print("$_\n");
+    #print"BEDROCK\n";
+
+    close(BEDROCK);
+
 
 }
+
+# $ perl OTHER.pl OTHER.pl
+
+# @ARGV = qw( first.txt OTHER.pl );  # Принудительное чтение трех файлов
+
+# while (<>) {
+#     #работает
+#     chomp;
+#     #   print "$_\n";
+#     my $fh;
+#
+#     open($fh, '>>', 'logfile') or die $!;
+#
+#
+#     #$_ = <STDIN>;
+#
+#     print($fh "$_\n");
+#
+#     close($fh);
+# }
+
+
+
+
+
+
+
+
+
+# sub logfile_name {
+#     chomp(@_);
+#     #open(FILE, '>', 'logfile') or die $!;
+#     open(BEDROCK, '>','logfile') or die $!;
+#
+#         $_ = <STDIN>;
+#
+#         print(BEDROCK "$_\n");
+#
+#
+#
+#
+# }
+#
+# logfile_name();
+
+# my @array = (0..10);
+# print @array;     # Вывод списка элементов
+# print "\n@array\n";   # Вывод строки (содержащей интерполируемый массив)
+# print reverse <>;
+# print (2+3)*4;
+
+sub print_format {
+    my ($user, $days_to_die) = @_;
+
+    print "Первым делом, введите ваш логин : ";
+    chomp($user = <STDIN>);
+    #print "\$user = $user\n";
+    $days_to_die = rand(10);
+
+    printf "Hello, %s; your password expires in %0.1f days!\n",
+        $user, $days_to_die;
+}
+
+
+#print_format();
+
+# my @items = qw( wilma dino pebbles );
+# my $format = "The items are:\n" . ("%13s\n" x @items);
+# print "the format is >>$format<<\n"; # Для отладки
+#printf $format, @items;
+#printf "The items are:\n".("%10s\n" x @items), @items;
+
+
