b0e4e2716fa02713ecacf4e498cb2c46971bd43b
chicagoist <le.legioner@gmail.com>
2021-11-18 16:30:18 +0200

    #@#$;#... всего лишь...

--- 5.1.pl
+++ 5.1.pl
@@ -3,13 +3,8 @@
 # File 5.1.pl
 # https://github.com/chicagoist/Exercises_From_LearningPerl.git
 # https://www.learning-perl.com/
+# https://www.linkedin.com/in/legioneroff/
 
-# Write a program that acts like cat  but reverses the order of the output lines.
-# (Some systems have a utility like this named tac.) If you run yours as ./tac fred
-# barney betty, the output should be all of file betty  from last line to first, then
-# barney, and then fred, also from last line to first. (Be sure to use the ./  in your
-# program’s invocation if you call it tac  so that you don’t get the system’s utility
-# instead!)
 
 use 5.10.0;
 use strict;
@@ -18,6 +13,13 @@ binmode(STDIN, ':utf8');
 binmode(STDOUT, ':utf8');
 use utf8;
 use warnings;
+
+# Write a program that acts like cat  but reverses the order of the output lines.
+# (Some systems have a utility like this named tac.) If you run yours as ./tac fred
+# barney betty, the output should be all of file betty  from last line to first, then
+# barney, and then fred, also from last line to first. (Be sure to use the ./  in your
+# program’s invocation if you call it tac  so that you don’t get the system’s utility
+# instead!)
 
 
 sub reverse_stdout {
