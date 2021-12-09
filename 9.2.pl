962bf9b7a48eb3ece4452ee64c59cf70a358593f
chicagoist <le.legioner@gmail.com>
2021-12-09 12:54:59 +0200

    Странное решение в книге

--- 9.2.pl
+++ 9.2.pl
@@ -24,15 +24,48 @@ use diagnostics;
 
 # Напишите программу, которая создает измененную копию текстового файла.
 # В копии каждое вхождение строки Fred (с любым регистром символов) должно
-# заменяться строкой Larry  (таким обраQ зом, строка Manfred Mann должна превратиться
+# заменяться строкой Larry  (таким образом, строка Manfred Mann должна превратиться
 # в ManLarry Mann). Имя входного файла должно задаваться в командной строке (не запрашивайте
 # его у пользователя!), а имя выходного файла образуется из того же имени и расширения .out.
 
+chomp(@_);
+$^I = ".out";
+while (<>) {
+    s/^\s*$//g; # remove empty lines
+    s/^\W*$//g; # remove lines without letters
+    s/fred/Larry/ig;
+    print;
+}
 
 
-# Не так изящно, как в книге, но так непонятно ставить ТЗ - это жесть.
-#
+# Странное решение в книге
+#  perl 9.2.pl sample_text
 
 
 # Верный ответ из книги:
+#Here’s one way to do it:
 #
+# my $in = $ARGV[0];
+# if (!defined $in) {
+#     die "Usage: $0 filename";
+# }
+# my $out = $in;
+# $out =~ s/(\.\w+)?$/.out/;
+# if (!open $in_fh, '<', $in) {
+#     die "Can't open '$in': $!";
+# }
+# if (!open $out_fh, '>', $out) {
+#     die "Can't write '$out': $!";
+# }
+# while (<$in_fh>) {
+#     s/Fred/Larry/gi;
+#     print $out_fh $_;
+# }
+
+# This program begins by naming its one and only command-line parameter,
+# and complaining if it didn’t get it. Then it copies that to $out and
+# does a substitution to change the file extension, if any, to .out.
+# (It would be sufficient, though, to merely append .out to the filename.)
+# Once the filehandles $in_fh  and $out_fh  are opened, the real program can
+# begin. If you didn’t use both options /g and /i, take off half a point,
+# since every fred and Fred should be changed.
