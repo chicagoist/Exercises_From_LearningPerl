551e9d6e914bd8670e178683718a03cc35783ec4
chicagoist <le.legioner@gmail.com>
2021-12-13 13:08:50 +0200

    fix

--- 9.5.01.pl
+++ 9.5.01.pl
@@ -1,26 +1,19 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 
-use 5.10.0;
 #use CGI;
 #use POSIX;
-# use Encode qw(decode_utf8);
-# use Encode qw(decode encode);
-# BEGIN{@ARGV=map Encode::decode($_,1),@ARGV;}
-# BEGIN{@ARGV = map decode_utf8($_, 1), @ARGV;}
-# use open qw(:std :encoding(UTF-8));
-# use utf8::all 'GLOBAL';
-# use Encode::Locale;
-# use Encode;
-#use diagnostics;
-
-
+#use utf8;
+use 5.10.0;
 use strict;
-use warnings FATAL => 'all';
-use utf8;
+use open qw(:std :encoding(UTF-8));
+use Encode qw(decode_utf8);
+BEGIN{@ARGV = map decode_utf8($_, 1), @ARGV;}
 binmode(STDIN, ':utf8');
 binmode(STDOUT, ':utf8');
-use DDP;
-use Data::Dumper;
+use warnings;
+use utf8::all 'GLOBAL'; # пробую этот модуль вместо закомментированных выше строк
+use Encode::Locale;
+use diagnostics;
 
 
 # File 9.5.pl
