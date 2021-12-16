f00c498cf5346d28941072ed7919c005a3f7aff9
chicagoist <le.legioner@gmail.com>
2021-12-16 17:06:43 +0200

    fix

--- 11.2.pl
+++ 11.2.pl
@@ -33,7 +33,7 @@ use Data::Dumper;
 # Write a program using Time::Moment to compute the interval between now and a date
 # that you enter as the year and month on the command line:
 #
-#Напишите программу, используя Time :: Moment, для вычисления интервала между текущим моментом и датой,
+# Напишите программу, используя Time :: Moment, для вычисления интервала между текущим моментом и датой,
 # которую вы вводите как год и месяц в командной строке:
 #
 # $ perl duration.pl 1960 9
@@ -41,8 +41,8 @@ use Data::Dumper;
 
 
 sub modules_time_moment {
-    chomp(@ARGV);
-    my ($year, $month ) = @ARGV;
+    chomp(@ARGV); #
+    my ($year, $month) = @ARGV;
 
     my $string = '1960-09-01';
     my $today;
@@ -53,16 +53,16 @@ sub modules_time_moment {
 
     $today = Time::Moment->now;
 
-    $delta = Time::Moment->new(year => $year,month => $month);
-    $result[0] = $delta->delta_years( $today );
-    $result[1] = $delta->delta_months( $today );
+    $delta = Time::Moment->new(year => $year, month => $month);
+    $result[0] = $delta->delta_years($today);
+    $result[1] = $delta->delta_months($today);
     $result[1] %= 12;
 
     printf("%s years %s months \n", @result);
 
 }
 
-    modules_time_moment();
+modules_time_moment();
 
 
 # Я два часа потратил на установку этого модуля. Так и не понял, что за ошибка, но Time инсталлировался,
