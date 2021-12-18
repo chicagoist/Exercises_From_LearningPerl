91a6cbd94effdc8a64c6e77f958c35971e3ddaaa
chicagoist <le.legioner@gmail.com>
2021-12-18 15:44:40 +0200

    fix

--- chapter12_examples.pl
+++ chapter12_examples.pl
@@ -23,7 +23,6 @@ use DDP;
 use Data::Dumper;
 
 
-
 # ОПЕРАТОРЫ ПРОВЕРКИ ФАЙЛОВ
 
 # Обратите внимание: содержимое $! не включается в сообщение die, потому что
@@ -34,5 +33,37 @@ warn "Config file is looking pretty old!\n" if -M '1.3.pl' > 1;
 
 #  Проверка существования файла осуществляется конструкцией –e:
 my $filename = '1.3.pl';
- die "Oops! A file called '$filename' already exists.\n" if -e $filename;
+ warn "Oops! A file called '$filename' already exists.\n\n" if -e $filename;
+
+#  So let’s go through our list of files to see which of them are larger than 100 K. But even if a
+# file is large, you shouldn’t warehouse it unless it hasn’t been accessed in the last 90 days
+# (so we know that it’s not used too often). The -s  file test operator, instead of returning true or false,
+# returns the file size in bytes (and an existing file might have 0 bytes):
+
+my @original_files = grep -f, <*>;
+my @big_old_files; # The ones we want to put on backup tapes
+foreach my $filename1 (@original_files) {
+    push @big_old_files, $filename1
+        if -s $filename1 > 10_000 and -M $filename1 > 5;
+}
+foreach (@big_old_files) {
+    print "Big and Old: $_\n";
+}
+
+# Если при проверке файла не указано имя файла или дескриптор (то есть при простом вызове вида –r или –s),
+# по умолчанию в качестве опеQ ранда используется файл, имя которого содержится в $_.
+foreach (@big_old_files) {
+    # my $size_in_K = -s /1000;      # Сюрприз! NOT WORK
+    my $size_in_k = (-s) / 1024;    # По умолчанию используется $_
+    # Конечно, явная передача параметра исключает любые недоразумения при проверке.
+    print "$_ is readable\n" if -r;  # same as -r $_
+    say "$_ = $size_in_k";
+}
+
+# ПРОВЕРКА НЕСКОЛЬКИХ АТРИБУТОВ ОДНОГО ФАЙЛА
+
+# Объединение нескольких файловых проверок позволяет создавать сложные логические условия.
+# Предположим, программа должна выполнить некую операцию только с файлами, доступными как
+# для чтения, так и для записи. Проверки атрибутов объединяются оператором and:
+if( -r $file and -w $file ) {... }
 
