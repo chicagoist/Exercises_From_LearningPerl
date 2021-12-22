0c8e4a6597a4e9fc859a144f4d78a9c2e6b550c6
chicagoist <le.legioner@gmail.com>
2021-12-22 14:35:34 +0200

    fix

--- 13.1.pl
+++ 13.1.pl
@@ -40,47 +40,32 @@ use Data::Dumper;
 
 =cut
 
+# Напишите программу, которая запрашивает у пользователя имя каталога и делает его текущим каталогом.
+# Если пользователь вводит строку из одних пропусков, по умолчанию программа должна перейти в домашний
+# каталог пользователя. После смены каталога выведите  его стандартное содержимое (за исключением
+# элементов, имена которых начинаются с точки) в алфавитном порядке.
+# (Подсказка: как это проще сделать – с дескриптором каталога или с глобом?)
+# Если попытка перехода в другой каталог завершилась неудачей, сообщите об этом пользователю,
+# но не пытайтесь выводить содержимое.
 
-state @files_O_R_W;
-sub files_group_check {
-    chomp(@ARGV);
+sub change_dir {
+    print "Please, change to directory : ";
 
-    foreach (@ARGV) {
-        if (-O -R -W $_) {
-            push @files_O_R_W, $_;
-        }
+    chomp(my $new_dir = <STDIN>);
+
+   #  say $new_dir;
+
+    chdir $new_dir;
+    opendir(my $dh, $new_dir) || die "Can't open $new_dir: $!";
+    while (readdir $dh) {
+        print "$new_dir/$_\n";
     }
-    # p @files_O_R_W;
-    foreach (@files_O_R_W) {
-        printf("File with -O -R -W : %s\n", $_);
-    }
+
 }
 
-files_group_check();
+change_dir();
 
 
 # Верный ответ из книги:
 
-# Here’s one way to do it: use v5.10; say "Looking for my files that are readable and writable";
-
-# die "No files specified!\n" unless @ARGV;
-# foreach my $file (@ARGV) {
-#     say "$file is readable and writable" if -o -r -w $file;
-# }
-
-# To use stacked file test operators, we need to use Perl 5.10 or later,
-# so we start with the use statement to ensure that we have the right version of Perl.
-# We die if there are no elements in @ARGV, and go through them with foreach otherwise.
-# We have to use three file test operators: -o to check if we own the file, -r to check
-# that it is readable, and -w  to check if it is writable. Stacking them as -o -r -w creates
-# a composite test that only passes if all three of them are true, which is exactly what we want.
-# If we wanted to do this with a version before Perl 5.10, it’s just a little more code.
-# The says become prints with added newlines, and the stacked file tests become separate tests
-# combined with the && short-circuit operator:
-
-# print "Looking for my files that are readable and writable\n";
-# die "No files specified!\n" unless @ARGV;
-# foreach my $file (@ARGV) {
-#     print "$file is readable and writable\n"
-#         if (-w $file && -r _ && -o _);
-# }
+#
