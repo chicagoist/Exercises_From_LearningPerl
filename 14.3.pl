627257c4913449a81ae939112ba28c0dcb0f8eee
chicagoist <le.legioner@gmail.com>
2021-12-30 15:28:10 +0200

    $string $sub_string

--- 14.3.pl
+++ 14.3.pl
@@ -23,12 +23,10 @@ use DDP;
 use Data::Dumper;
 
 
-
 # File 14.3.pl
 # https://github.com/chicagoist/Exercises_From_LearningPerl.git
 # https://www.learning-perl.com/
 # https://www.linkedin.com/in/legioneroff/
-
 
 
 # Напишите программу, которая ищет в заданной строке все вхождения
@@ -44,15 +42,42 @@ sub sub_string {
     print "Enter a substring: ";
     chomp(my $sub_string = <STDIN>);
 
+    my $first_target_point = index($string, $sub_string);
+    my $last_target_point = rindex($string, $sub_string);
 
+    if ($first_target_point != -1 && $last_target_point != -1) {
+        if ($first_target_point == $last_target_point ) {
+            print "The match point substring '$sub_string' in string '$string' => $first_target_point\n";
+        }elsif ($first_target_point != $last_target_point ) {
+            print "The first match point substring '$sub_string' in string '$string' => $first_target_point\n";
+            print "The last match point substring '$sub_string' in string '$string' => $last_target_point\n";
+        }
+
+    }
+
+    if ($first_target_point != -1 && $last_target_point == -1) {
+        print "The first match point substring '$sub_string' in string '$string' => $first_target_point\n";
+    }
+
+    if ($first_target_point == -1 &&  $last_target_point != -1) {
+        print "The last match point substring '$sub_string' in string '$string' => $last_target_point\n";
+    }
+
+    if ($first_target_point == -1 &&  $last_target_point == -1) {
+        print "The NO MATCH point substring '$sub_string' in string '$string'\n";
+    }
 
 }
+
 
 sub_string();
 
 =begin text
 
  $ perl 14.3.pl
+
+ OUTPUT:
+ The match point substring 'T' in string 'This is a test.' => 0
 
 
 =end text
