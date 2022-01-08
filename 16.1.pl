1e11f9942d100618430b5c814d8231d1e18e527d
chicagoist <le.legioner@gmail.com>
2022-01-08 14:16:10 +0200

    Chapter 16

---
+++ 16.1.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl -w
+
+use 5.10.0;
+# use CGI;
+# use Encode qw(decode_utf8);
+# use Encode qw(decode encode);
+# BEGIN{@ARGV=map Encode::decode($_, 1),@ARGV;}
+# BEGIN{@ARGV = map decode_utf8($_, 1), @ARGV;}
+# use open qw(:std :encoding(UTF-8));
+# use Encode::Locale;
+# use Encode;
+# use Time::Moment;
+# use diagnostics;
+
+
+use strict;
+use warnings FATAL => 'all';
+use utf8;
+binmode(STDIN, ':utf8');
+binmode(STDOUT, ':utf8');
+use utf8::all 'GLOBAL';
+use DDP;
+use Data::Dumper;
+use Bundle::Camelcade;
+
+
+
+# File 16.1.pl
+# https://github.com/chicagoist/Exercises_From_LearningPerl.git
+# https://www.learning-perl.com/
+# https://www.linkedin.com/in/legioneroff/
+
+
+#
+
+
+
+
+
+=begin text
+
+ $ perl 16.1.pl
+
+
+
+
+=end text
+
+=cut
+
+
+# Верный ответ из книги:
+
+#
