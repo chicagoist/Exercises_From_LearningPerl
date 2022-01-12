c2de420e7fb1002fe22f471eacb826b72890adb2
chicagoist <le.legioner@gmail.com>
2022-01-12 13:09:57 +0200

    $SIG{'INT'} = 'my_int_handler_flag';

--- chapter16_after_exec_examples.pl
+++ chapter16_after_exec_examples.pl
@@ -26,7 +26,8 @@ use POSIX;
 
 
 
-{ # ПЕРЕМЕННЫЕ СРЕДЫ
+{
+    # ПЕРЕМЕННЫЕ СРЕДЫ
 
     # В Perl для работы с переменными среды используется специальный хеш %ENV;
     # каждый ключ хеша соответствует одной переменной. В начале выполнения
@@ -52,7 +53,8 @@ use POSIX;
 
 }
 
-{ # ОБРАТНЫЕ АПОСТРОФЫ И СОХРАНЕНИЕ ВЫВОДА
+{
+    # ОБРАТНЫЕ АПОСТРОФЫ И СОХРАНЕНИЕ ВЫВОДА
 
     # При использовании обеих функций system и exec
     # выходные данные запущенной команды направляются
@@ -71,7 +73,7 @@ use POSIX;
     # Например, для получения документации по  функциям Perl мы могли бы
     # многократно вызвать perldoc с разными аргументами:
 
-    my @functions = qw{ int rand sleep length hex eof not exit sqrt umask };
+    my @functions = qw{int rand sleep length hex eof not exit sqrt umask};
     my %about;
     foreach (@functions) {
         $about{$_} = `perldoc -t -f $_`;
@@ -84,15 +86,15 @@ use POSIX;
     # Постарайтесь обходиться без обратных апострофов в тех местах, где
     # вывод не сохраняется. Пример:
     print "Starting the frobnitzigator:\n";
-    my $output_with_errors = `frobnitz -enable 2>&1`;  # Не делайте этого!
-    `frobnitz -enable`;  # Не делайте этого!
+    my $output_with_errors = `frobnitz -enable 2>&1`; # Не делайте этого!
+    `frobnitz -enable`;                               # Не делайте этого!
     print "Done!\n";
     say "\$output_with_errors => $output_with_errors";
 
     # Итак, и с точки зрения эффективности, и с точки зрения безопасности
     # system оказывается предпочтительнее.
 
- }
+}
 
 {
     # Обратные апострофы в списочном контексте
@@ -124,7 +126,8 @@ use POSIX;
     # }
 }
 
-{# ПРОЦЕССЫ КАК ФАЙЛОВЫЕ ДЕСКРИПТОРЫ
+{
+    # ПРОЦЕССЫ КАК ФАЙЛОВЫЕ ДЕСКРИПТОРЫ
     #
     open LS, "date|" or die "cannot pipe to mail: $!";
     open PRINT, "|grep 2022" or die "cannot pipe to mail: $!";
@@ -136,7 +139,6 @@ use POSIX;
     #
     print "\$date_comand => ", <$date_comand>;
     print "LS =>  ", <LS>;
-
 
     close LS;
     close $date_comand;
@@ -157,10 +159,10 @@ use POSIX;
     # written with backquotes, you would not see any output until the find  command had completely finished,
     # and it’s comforting to see that it’s actually doing the job even before it’s done.
 
-
 }
 
-{ # ВЕТВЛЕНИЕ
+{
+    # ВЕТВЛЕНИЕ
 
     # Кроме высокоуровневых интерфейсов, описанных ранее, Perl
     # предоставляет практически прямой доступ к низкоуровневым
@@ -172,10 +174,90 @@ use POSIX;
     defined(my $pid = fork) or die "Cannot fork: $!";
     unless ($pid) {
         # Дочерний процесс
-         exec "echo 'Date today => ' `date`"; die "cannot exec date: $!";
+        exec "echo 'Date today => ' `date`";
+        die "cannot exec date: $!";
     }
     # Родительский процесс
     waitpid($pid, 0);
 
+}
 
- }
+{
+    # ОТПРАВКА И ПРИЕМ СИГНАЛОВ
+    defined(my $pid = fork) or die "Cannot fork: $!";
+    # Вы можете отправить из процесса Perl сигнал другому процессу, но для этого
+    # необходимо знать идентификатор целевого процесса. Определить его не так просто,
+    # но предположим, вы хотите отправить сигнал SIGINT процессу 4201. Делается это
+    # достаточно просто:
+    # kill 2, 17906 or die "Cannot signal 17906 with SIGINT: $!";
+
+    # Таким образом, зондирование процесса может выглядеть примерно так:
+    # unless (kill 0, $pid) {
+    #     warn "$pid has gone away!";
+    # }
+
+    # Допустим, ваша программа создает временные файлы в каталоге /tmp, которые удаляются в
+    # конце работы программы. Если кто-то нажмет Control-C во время выполнения, при аварийном
+    # завершении в /tmp останется «мусор», а это крайне невежливо. Проблема решается обработчиком
+    # сигнала, который позаботится об очистке:
+    my $temp_directory = "/tmp/myprog.$$";
+    # # Каталог для временных файлов
+        mkdir $temp_directory, 0700 or die "Cannot create $temp_directory: $!";
+    sub clean_up {
+        unlink glob "$temp_directory/*";
+        die "interrupted, exiting...\n" if rmdir $temp_directory;
+    }
+    sub my_int_handler {
+        &clean_up;
+    }
+    $SIG{'INT'} = 'my_int_handler';
+    # # Время идет, программа работает, в каталоге создаются
+    # # временные файлы. Потом кто-то нажимает Control-C.
+    # # Конец нормального выполнения
+    foreach (sort keys %SIG) {
+        if ($SIG{$_}) {
+            print "$_ =  $SIG{$_}\n";
+        }
+        else {
+            print "$_ =  uninitialized\n";
+        }
+    }
+
+    open my $find_fh, '-|', 'find',
+        qw(/ -atime +90 -size +1000 -print) or die "cannot pipe from find: $!";
+    while (<$find_fh>) {
+        chomp;
+        printf "%s size %dK last accessed %.2f days ago\n", $_, (1023 + -s $_) / 1024, -A $_;
+    }
+    &clean_up;
+
+
+    # Допустим, обработка каждой строки файQ ла занимает несколько секунд, и вы хотите отменить
+    # общую обработку при получении сигнала прерывания, но только не на середине обработки строки.
+    # Установите флаг в обработчике прерывания и проверяйте его состояние в конце обработки каждой строки:
+    my $int_count;
+    $int_count = 0;
+    sub my_int_handler_flag {
+        $int_count++;
+    }
+    $SIG{'INT'} = 'my_int_handler_flag';
+
+
+    open my $find_windows, '-|', 'find',
+        qw(/mnt/c/Users/vdundukov/ -atime +10 -size +1000 -print) or die "cannot pipe from WINDOWS USER find: $!";
+    while (<$find_windows>) {
+        chomp;
+        if ($int_count) {
+            # Получен сигнал прерывания !
+            print "[processing interrupted WINDOWS USER...]\n";
+            last;
+        }else {
+            printf ("%s size %dK last accessed %.2f days ago in WINDOWS USER\n", $_, (1023 + -s $_) / 1024, -A $_);
+        }
+    }
+
+    # Итак, при получении сигнала можно либо установить флаг, либо завершить работу программы; собственно,
+    # на этом возможности обработки сигналов заканчиваются. Впрочем, текущая реализация обработчиков
+    # сигналов неидеальна постарайтесь свести объем выполняемых действий  к минимуму, или ваша программа
+    # может «упасть» в самый неподходящий момент.
+}
