bd8c9e0b6b7666a1648cb9bff57c3f82d25740af
chicagoist <le.legioner@gmail.com>
2022-01-13 16:57:13 +0200

    Chapter 17

---
+++ chapter17_examples.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+use 5.10.0;
+# use CGI;
+use POSIX;
+# use Encode qw(decode_utf8);
+# use Encode qw(decode encode);
+# BEGIN{@ARGV=map Encode::decode($_,1),@ARGV;}
+# BEGIN{@ARGV = map decode_utf8($_, 1), @ARGV;}
+# use open qw(:std :encoding(UTF-8));
+# use utf8::all 'GLOBAL';
+# use Encode::Locale;
+# use Encode;
+# use diagnostics;
+
+
+use strict;
+use warnings FATAL => 'all';
+use utf8;
+binmode(STDIN, ':utf8');
+binmode(STDOUT, ':utf8');
+use utf8::all 'GLOBAL';
+use DDP;
+use Data::Dumper;
+use Bundle::Camelcade;
+
+
+# РАСШИРЕННЫЕ ВОЗМОЖНОСТИ Perl
+
+{
+    # ПЕРЕХВАТ ОШИБОК В БЛОКАХ eval
+    1;
+
+}
+
+
+
