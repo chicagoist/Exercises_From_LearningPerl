997ef6a85d49b62f1a9c3bab78e382c3987efb7e
chicagoist <le.legioner@gmail.com>
2022-01-20 15:35:57 +0200

    if ($_ =~ /$template/ig)

--- 17.1.pl
+++ 17.1.pl
@@ -24,25 +24,70 @@ use Data::Dumper;
 use Bundle::Camelcade;
 
 
-
 # File 17.1.pl
 # https://github.com/chicagoist/Exercises_From_LearningPerl.git
 # https://www.learning-perl.com/
 # https://www.linkedin.com/in/legioneroff/
 
 
-#
+# Напишите программу, которая читает список строк из файла (по одной строке).
+# Затем пользователь вводит шаблоны, совпадения которых ищутся в строках.
+# Для каждого шаблона программа должна сообщить количество строк, содержащих
+# совпадения, а также вывести сами строки. Файл не должен читаться заново для каждого шаблона;
+# храните строки в памяти. Имя файла можно жестко закодировать в программе. Если шаблон
+# недействителен (например, если он содержит непарные круглые скобки), программа должна просто
+# вывести сообщение об ошибке и разрешить пользователю ввести другой шаблон. Если пользователь
+# вместо шаблона вводит пустую строку, программа завершает работу.
 
-sub date_out {
+sub search_templates {
+    my @sample_text;
+    my @matching_lines;
+    my $index = 0;
 
+    open my $fh_sample_text, '<:encoding(UTF-8)', 'examples/sample_files/text_files/sample_text'
+        or die "Cannot open file for read: $!"; # open file with '<:encoding(UTF-8)' for cyrillic too.
+
+    while (<$fh_sample_text>) { # loop for opened file
+        s/^\s+(\.*)$/$1/; # cut spaces from beginning line
+        push @sample_text, $_; # push to new array
+    }
+    close($fh_sample_text); # close opened file
+
+    print "Enter your template: "; # template
+    chomp(my $template = <STDIN>) or die "Not correct template. Stop the app"; # like wilma, fred, barney or other text
+
+    if (!$template) { # die if template is empty
+        die "Empty template. Stop the app";
+    }
+    else { # if template is not empty
+
+        foreach (@sample_text) { # loop for array with lines
+            if ($_ =~ /$template/ig) { # if template match for one line from array
+                #print "$_\n"; # for debugging
+                push @matching_lines, $_; # push line with matched template
+                $index++; # increment index for each matching
+            }
+        }
+
+        print "@matching_lines"; # print array with matched lines
+        print "Matching template = $index\n"; # print index
+    }
 }
-&date_out;
+
+&search_templates;
+
 
 
 =begin text
 
  $ perl 17.1.pl
-
+ Enter your template: wilma
+ wilma saw this and said to betty, "you should ask barney to take
+ ...
+  the telephone rang. "you get that, fred," said wilma. "i'll be
+ Совпадет ли он в строке Mrs. Wilma Flintstone?
+ Будет ли он совпадать с wilma, но не с barney?
+ Matching template = 23
 
 =end text
 
