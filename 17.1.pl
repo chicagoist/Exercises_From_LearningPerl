8cb1729f5d8d02ecbf939c37ef34047842ebeead
chicagoist <le.legioner@gmail.com>
2022-01-20 15:41:17 +0200

    if ($_ =~ /$template/ig)

--- 17.1.pl
+++ 17.1.pl
@@ -96,4 +96,21 @@ sub search_templates {
 
 # Верный ответ из книги:
 
-#
+# Here’s one way to do it:
+# my $filename = 'path/to/sample_text';
+# open my $fh, '<', $filename or die "Can't open '$filename': $!";
+# chomp(my @strings = <$fh>);
+# while (1) {
+#     print 'Please enter a pattern: ';
+#     chomp(my $pattern = <STDIN>);
+#     last if $pattern =~ /\A\s*\Z/;
+#     my @matches = eval {grep /$pattern/, @strings;};
+#     if ($@) {
+#         print "Error: $@";
+#     }
+#     else {
+#         my $count = @matches;
+#         print "There were $count matching strings:\n", map "$_\n", @matches;
+#     }
+#     print "\n";
+# }
